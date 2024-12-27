57233314b1648e92910b0e379b2c9631c6da18de
eshaan <eshdesh@null.net>
2024-12-26 20:19:49 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+echo "THESE ARE MY DOSBOX SETTINGS, CHANGE YOURS IF NEEDED:
+fullscreen=true
+fulldouble=false
+fullresolution=2560x1600 # my resolution
+windowresolution=2560x1600
+output=opengl
+autolock=true
+sensitivity=100
+waitonerror=true
+priority=higher,normal
+mapperfile=mapper-0.74-3.map
+usescancodes=true"
+
+python3 -m venv "venv"
+source venv/bin/activate
+pip install getpass
+python3 setup.py
---
+++ setup.py
@@ -0,0 +1,21 @@
+dospath = input("input the path for your preffered drive of your dos emulation software (e.g. /home/username/dos): ")
+
+import getpass
+username = getpass.getuser()
+dospath = dospath.replace("~","/home/" + username)
+
+import os
+
+os.environ['PATH'] += ':/bin:/usr/bin'
+
+os.system("touch " + dospath + "/INPUT.TXT")
+os.system("touch " + dospath + "/OUTPUT.TXT")
+
+os.system('{ echo "c = ' + dospath + '"; cat ../backend/main.py; } > temp && mv temp ../backend/main.py')
+
+os.system("mv ../backend ../execs/* " + dospath)
+
+backpath = dospath + "/backend"
+
+os.system('''printf '\nalias dosemu-linux="dosbox & python3 ''' + backpath + '''/main.py & wait"' >> ~/.bashrc''')
+os.system('''printf '\nalias dosemu-linux="dosbox & python3 ''' + backpath + '''/main.py & wait"' >> ~/.zshrc''')
