79095bd11a493bbc830e453eb23ad9e233c92ab3
eshaan <eshdesh@null.net>
2024-12-28 00:38:23 -0600

    Add files via upload

---
+++ gfx/EXAMPLE.C
@@ -0,0 +1,34 @@
+// THIS EXAMPLE FILE HAS EVERY FUNCTION THAT YOU PROBABLY NEED TO USE FOR OUR PROJECT
+// btw if someone else is actually using this code, please ignore most of the comments, they are for my groupmates. ty!
+
+#include <graphics.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <dos.h>
+
+#include "PIXEL.C"
+
+void keygrab(int key){
+    if (key == 52) { 
+        outtextxy(100,100,"you pressed 4. i am so proud of you :,)");
+    }   
+    // add ifs here for keyboard clicks. it's using ascii code soooo good luck
+    // i added the ascii key to resources/ascii.png to save you some time
+    // you can do it!!! call me if u need help, i have no life
+}
+
+void mousedown(int x, int y){
+    px(x, x+5, y, y+5, "red");
+}
+
+#include "CURSOR.C"
+
+
+int main() {
+    init();
+    px(50, 200, 50, 200, "blue");
+    printf(getpx(70, 70));
+    cursor();
+    
+    return 0;
+}
