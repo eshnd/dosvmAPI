813546a81867c1ba267b525855a648f341c1fe39
eshaan <eshdesh@null.net>
2024-12-27 22:43:45 -0600

    Add files via upload

---
+++ linux/SRS.C
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <dos.h>
+#include <conio.h>
+#include <stdlib.h>
+
+int shell(char* doppy) {
+    FILE *file;
+    char doppy[100];
+
+    file = fopen("C:\\INPUT.TXT", "w");
+
+    if (strcmp(doppy, "exit") == 0){
+        return 0;
+    }
+
+    fprintf(file, "%s\n", doppy);
+
+    fclose(file);
+
+    delay(2000);
+
+    file = fopen("C:\\OUTPUT.TXT", "r");
+
+    while (fgets(doppy, sizeof(doppy), file) != NULL) {
+        printf("%s", doppy);
+    }
+    
+
+    fclose(file);
+
+}
