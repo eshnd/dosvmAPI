404d765f76bef171c9291b8ddb054bea51b10c2c
eshaan <eshdesh@null.net>
2025-01-16 08:59:17 -0600

    Update SRS.C

--- linux/SRS.C
+++ linux/SRS.C
@@ -1,19 +1,13 @@
-// why so serious? jonkler appears and starts doing silly dances. i am so sleepy its like 14 am here :D
 #include <stdio.h>
-#include <dos.h>
-#include <conio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dos.h>
 
-char* shell(char* doppy) {
+char* shell(char* doppy, int mem) {
     FILE *file;
-    char *output = malloc(1);  // have to use mallocs ........ i hate c
-    output[0] = '\0';
-
-    if (strcmp(doppy, "exit") == 0) {
-        free(output);
-        return "exit";
-    }
+    char temp[256];
+    char *out2 = malloc(mem);
+    out2[0] = '\0';
 
     file = fopen("C:\\INPUT.TXT", "w");
     fprintf(file, "%s\n", doppy);
@@ -22,16 +16,17 @@ char* shell(char* doppy) {
     delay(2000);
 
     file = fopen("C:\\OUTPUT.TXT", "r");
-
-    while (fgets(doppy, 256, file) != NULL) {
-        size_t current_length = strlen(output);
-        size_t new_length = current_length + strlen(doppy) + 1;
-        output = realloc(output, new_length); // c++ >>>>>
-        strcat(output, doppy);
+    while (fgets(temp, 256, file)) {
+        strcat(out2, temp);
     }
-
     fclose(file);
 
-    return output;
-} // this will always appear with abnormal program termination and null pointer assignment, but it still works so fuck it lol
-// if someone manages to fix that i would be super appreciative :DDD
+    return out2;
+}
+
+int main() {
+    char *result = shell("ls", 1024);
+    printf("%s", result);
+    free(result);
+    return 0;
+}
